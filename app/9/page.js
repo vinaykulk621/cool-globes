d139a80fa48676f3bb1b99af7205971e537fcea9
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-20 20:56:03 +0530

    Please Work Bro

--- app/9/page.js
+++ app/9/page.js
@@ -1,11 +1,9 @@
 import { Links } from "../../components/Links/Links";
-import EarthNight_18 from "../../components/Cool-Ones/EarthNight_18";
 import EarthNight_19 from "../../components/No-Ball/EarthNight_19";
 
 export default function Home() {
   return (
     <>
-      <EarthNight_18 />
       <EarthNight_19 />
       <Links
         prev={"8"}
