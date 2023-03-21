355254b519c244626bf724ebe5872ad99902faab
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-21 20:37:51 +0530

    Git Link

--- components/Links/Links.js
+++ components/Links/Links.js
@@ -1,3 +1,5 @@
+import { GithubSVG } from "./githubSVG";
+
 export function Links({ prev, next }) {
   return (
     <div className="m-5 flex items-center justify-around font-spotify text-2xl text-white">
@@ -6,6 +8,7 @@ export function Links({ prev, next }) {
         className="rounded-lg bg-gray-600 p-4 hover:bg-black">
         Previous
       </a>
+      <GithubSVG />
       <a
         href={`/${next}`}
         className="rounded-lg bg-gray-600 p-4 hover:bg-black">
