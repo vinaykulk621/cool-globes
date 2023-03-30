14af8fb8aed1d8bc5d737414352b03e83c952338
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-30 11:16:09 +0530

    Updated Links

--- app/17/page.js
+++ app/17/page.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <>
       <EarthNight_19 />
-      <Links
-        prev={"16"}
-        next={"/"}
-      />
+      <Links prev={"16"} />
     </>
   );
 }
